b931e1b41100a61455bab243c7338fef42f9f953
Sean Leas <phoenix_24leas.com>
2020-06-29 19:47:21 -0600

    Git will not let me continue unless turn-test.js is committed

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,11 +5,7 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card')
 
 describe('Turn', function() {
-  this.beforeEach(function () {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card)
 
-  }) 
   it('should be a function', function() {
     const turn = new Turn
     expect(Turn).to.be.a('function')
@@ -49,6 +45,17 @@ describe('Turn', function() {
 
   })
 
+  it('Should have a method that returns either "correct" or "incorrect"', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card)
+
+    var guess = turn.returnGuess()
+    var result1 = turn.evaluateGuess(guess)
+    var message = turn.giveFeedback(result1)
+    expect(message).to.equal('Correct!')
+  })
+
+  
 })
 // describe('Card', function () {
 
